1e964746d827d461e210651f37bef5ba7a07adc4
Unagui19 <francorr199@gmail.com>
2023-08-24 14:30:07 -0300

    Uso de API WEB

--- Program.cs
+++ Program.cs
@@ -2,45 +2,126 @@
 using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Net;
+using monedaSpace;
 
-string documento;
-using (var fs = new FileStream("platos.json",FileMode.Open))//abrir el archivo
+var url = $"https://api.coindesk.com/v1/bpi/currentprice.json";
+var request = (HttpWebRequest) WebRequest.Create(url);
+request.Method="GET";
+request.ContentType="application/json.";
+request.Accept="application/json";
+
+try
 {
-    using(var sr = new StreamReader(fs))// para leer
+    using (WebResponse response = (HttpWebResponse)request.GetResponse())//obtengo la respuesta del recurso online
     {
-       documento= sr.ReadToEnd(); 
-       sr.Close();
+        using (var sr = new StreamReader (response.GetResponseStream()))//convierte la respuesta en un un stream (flujo de datos)
+        {
+            if (sr != null)
+            {
+                string text = sr.ReadToEnd(); //convierto el sr en un string
+                Moneda? moneda = JsonSerializer.Deserialize<Moneda>(text);    //para pasar lo obtenido del json a los atributos de mi clase
+
+                Console.WriteLine("PRECIOS DE BITCOIN EN DIFERENTES MONEDAS: ");
+                Console.WriteLine("\nUSD= U$S"+moneda.bpi.USD.rate);
+                Console.WriteLine("\nEUR= €"+moneda.bpi.EUR.rate);
+                Console.WriteLine("\nGBR= £"+moneda.bpi.GBP.rate);
+                Console.WriteLine("");
+                Console.WriteLine("Ingrese un tipo de cambio");
+                Console.WriteLine("\n1- USD");
+                Console.WriteLine("\n2- EUR");
+                Console.WriteLine("\n3- GBR");
+                Console.WriteLine("");
+                int opcion;
+                bool ingreso = int.TryParse(Console.ReadLine(),out opcion);//asi ingreso el valor, si es true cambiaba el valor de opcion
+                if (ingreso && opcion>=1 && opcion<=3)
+                {
+                    switch (opcion)
+                    {
+                        case 1: 
+                
+                                Console.WriteLine("\nCode: "+moneda.bpi.USD.code);
+                                Console.WriteLine("\n Description: "+moneda.bpi.USD.description);
+                                Console.WriteLine("\n Symbol: "+moneda.bpi.USD.symbol);
+                                Console.WriteLine("\n Rate: "+moneda.bpi.USD.rate);
+                                Console.WriteLine("\n Rate_float: "+moneda.bpi.USD.rate_float);
+                                break;
+                        case 2: 
+                                Console.WriteLine("\nCode: "+moneda.bpi.EUR.code);
+                                Console.WriteLine("\n Description: "+moneda.bpi.EUR.description);
+                                Console.WriteLine("\n Symbol: "+moneda.bpi.EUR.symbol);
+                                Console.WriteLine("\n Rate: "+moneda.bpi.EUR.rate);
+                                Console.WriteLine("\n Rate_float: "+moneda.bpi.EUR.rate_float);
+                                break;
+                        default:
+                                Console.WriteLine("\nCode: "+moneda.bpi.GBP.code);
+                                Console.WriteLine("\n Description: "+moneda.bpi.GBP.description);
+                                Console.WriteLine("\n Symbol: "+moneda.bpi.GBP.symbol);
+                                Console.WriteLine("\n Rate: "+moneda.bpi.GBP.rate);
+                                Console.WriteLine("\n Rate_float: "+moneda.bpi.GBP.rate_float);
+                                break;
+                    }
+                }
+                else{
+                    Console.WriteLine("opcion invalida");
+                }
+
+                Console.WriteLine("Hora actualizada: "+moneda.time.updated);
+            }
+        }
+
     }
-    fs.Close();     
+}
+catch (WebException)
+{
+    Console.WriteLine("Problemas de acceso a la API");
+    throw;
 }
 
-Console.WriteLine(documento);
-List<Platos>? plato = JsonSerializer.Deserialize<List<Platos>>(documento);
-Console.WriteLine($"Nombre: {plato[0].Nombre}");
-Console.WriteLine($"Precio: {plato[0].Precio}");
-Console.WriteLine($"stock: {plato[0].Stock}");
 
-// List<Platitos>? listadoPlatos = JsonSerializer.DeserializeAsync<List<Platitos>>(documento);
-// Console.WriteLine(""+plato.Nombre);
 
-foreach (var item in plato)
-{   
-    Console.WriteLine($"{item.Nombre}");
-    Console.WriteLine($"{item.Precio}");
-    Console.WriteLine($"{item.Stock}");
-    Console.WriteLine("\n");
-}//lo hiceeeeeeeeee, asi se lee un json!!!
 
-var fst = new FileStream("platos.json",FileMode.Create);
-Platos LALA = new Platos("milanga",4000,6);
-plato.Add(LALA);
-string acrchivoJson = JsonSerializer.Serialize(plato);
-using (var sw =new StreamWriter(fst))
-{
-    sw.WriteLine(acrchivoJson);
-    sw.Close();
-}//PARA CREAR UN JSON y guardar o solo guardar y funcionooooooooooo giiiiiiiiiiiiiiil
-fst.Close();
+
+
+// string documento; 
+// using (var fs = new FileStream("platos.json",FileMode.Open))//abrir el archivo
+// {
+//     using(var sr = new StreamReader(fs))// para leer
+//     {
+//        documento= sr.ReadToEnd(); 
+//        sr.Close();
+//     }
+//     fs.Close();     
+// }
+
+// Console.WriteLine(documento);
+// List<Platos>? plato = JsonSerializer.Deserialize<List<Platos>>(documento);
+// Console.WriteLine($"Nombre: {plato[0].Nombre}");
+// Console.WriteLine($"Precio: {plato[0].Precio}");
+// Console.WriteLine($"stock: {plato[0].Stock}");
+
+// // List<Platitos>? listadoPlatos = JsonSerializer.DeserializeAsync<List<Platitos>>(documento);
+// // Console.WriteLine(""+plato.Nombre);
+
+// foreach (var item in plato)
+// {   
+//     Console.WriteLine($"{item.Nombre}");
+//     Console.WriteLine($"{item.Precio}");
+//     Console.WriteLine($"{item.Stock}");
+//     Console.WriteLine("\n");
+// }//lo hiceeeeeeeeee, asi se lee un json!!!
+
+// var fst = new FileStream("platos.json",FileMode.Create);
+// Platos LALA = new Platos("milanga",4000,6);
+// plato.Add(LALA);
+// var options = new JsonSerializerOptions { WriteIndented = true };//para darle un formato mas limpio al json
+// string acrchivoJson = JsonSerializer.Serialize(plato,options);
+// using (var sw =new StreamWriter(fst))
+// {
+//     sw.WriteLine(acrchivoJson);
+//     sw.Close();
+// }//PARA CREAR UN JSON y guardar o solo guardar y funcionooooooooooo giiiiiiiiiiiiiiil
+// fst.Close();
 
 
 
